fb31379b9e28a7eb8369ecdf94089a261025bf21
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-08-17 19:20:24 +0530

    Pricing Section Complite

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -9,9 +9,38 @@ const PricingSection = () => {
         Pricing
       </h2>
       <div className="flex flex-wrap">
-        {pricingOptions.map((oprion, index) => (
+        {pricingOptions.map((option, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
-            <div className="p-10 border border-neutral-700 rounded-xl"></div>
+            <div className="p-10 border border-neutral-700 rounded-xl">
+              <p className="text-4xl mb-8">
+                {option.title}{" "}
+                {option.title === "Pro" && (
+                  <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text text-xl mb-4 ml-2">
+                    (Most Populer)
+                  </span>
+                )}
+              </p>
+              <p className="mb-8">
+                <span className="text-5xl mt-6 mr-2"> {option.price}</span>
+                <span className="text-neutral-400 tracking-tight">/Month</span>
+              </p>
+              <ul>
+                {option.features.map((feature, index) => (
+                  <div key={index}>
+                    <li className="mt-8 flex items-center ">
+                      <CheckCircle2 />
+                      <span className="ml-2"> {feature}</span>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+              <a
+                href="#"
+                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 border-orange-900 border rounded-lg tracking-tight text-xl hover:bg-orange-900 translate duration-200"
+              >
+                Subscribe
+              </a>
+            </div>
           </div>
         ))}
       </div>
