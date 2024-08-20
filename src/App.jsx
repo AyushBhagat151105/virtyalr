9bf6c734f9bf27f23551e891d7b6a06329ed51e5
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-08-20 15:21:09 +0530

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
-        {/* <HeroSection /> */}
+        <HeroSection />
         <FeatureSection />
         <Workflow />
         <PricingSection />
